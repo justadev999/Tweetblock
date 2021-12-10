04b4d3d6b71c98717ab5cc666727fe93135f5dba
Simone <bucciolsimone@gmail.com>
2021-12-10 22:36:33 +0100

    style: reset style

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,19 @@ import type { NextPage } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import styles from '../styles/Home.module.css';
+import Router from 'next/router';
 
-const Home: NextPage = () => (
-  <div>
-    <button>Home</button>
-  </div>
-);
+const Home: NextPage = () => {
+  const handleClick = (e: any, page: any) => {
+    e.preventDefault();
+    console.log('i clicked a page');
+  };
+  console.log();
+  return (
+    <div>
+      <button onClick={(e) => handleClick(e, 'blog')}>Home</button>
+    </div>
+  );
+};
 
 export default Home;
